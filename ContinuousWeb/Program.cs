1c02bfdd1304cd5aaf08c5301f571697487c3338
Tarak-Chandra-Sarkar <tarak.chandra.sarkar@hotmail.com>
2017-07-10 21:01:23 +0530

    Specified Hosting Port to http://localhost:5001

--- ContinuousWeb/Program.cs
+++ ContinuousWeb/Program.cs
@@ -13,6 +13,7 @@ namespace ContinuousWeb
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+				.UseUrls("http://localhost:5001")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
